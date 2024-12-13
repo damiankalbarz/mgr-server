e2bbdb2979ab1d38ea68083fb80dd2fd1b74a787
Damian <kalbarzd@gmail.com>
2024-12-13 15:19:28 +0100

    first commit

---
+++ src/main/java/com/example/mgr_server/MgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mgr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgrServerApplication.class, args);
+	}
+
+}
